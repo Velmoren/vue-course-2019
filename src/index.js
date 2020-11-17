68c437d5585a2344bf7efba0d286be7a5ce891fd
Aleksander <ilyasov_as@creacept.ru>
2020-11-17 17:50:15 +0300

    webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './js/common'
+import './css/main.css'
+import './scss/main.scss'
+
+//первый способ
+// import 'vue'
+//второй способ
+// import 'bootstrap/dist/css/bootstrap.css'
+//третий способ
+window.Vue = require('vue')
+
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    //точка входа
+    entry: {
+        app: './src/index.js'
+    },
+    //точка выхода
+    output: {
+        //имя файла (если у нас будет несколько точек входа)
+        filename: "[name].js",
+        //путь
+        path: path.resolve(__dirname, './dist'),
+        //пусть к папке dist
+        publicPath: "/dist"
+    },
+    module: {
+        //правила компиляции
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            esModule: false,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [
+                                    require('autoprefixer'),
+                                    require('css-mqpacker'),
+                                    require('cssnano')({
+                                        preset: [
+                                            'default', {
+                                                discardComments: {
+                                                    removeAll: true
+                                                }
+                                            }
+                                        ]
+                                    })
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {sourceMap: true}
+                    },
+                ],
+            }
+        ]
+    },
+    devServer: {
+        //оверлей с ошибками прямо на экране
+        overlay: true
+    },
+    //подключение плагинов
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css'
+        })
+    ]
+}
