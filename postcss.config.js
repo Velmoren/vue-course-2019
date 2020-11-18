91f4ca62af76216bd2152a66bd43d58bbac64c48
Aleksander <ilyasov_as@creacept.ru>
2020-11-18 17:21:04 +0300

    webpack 5

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,7 @@
-/*
-  autoprefixer - https://github.com/postcss/autoprefixer
-  cssnano - https://github.com/hail2u/node-css-mqpacker
-  css-mqpacker - HAS BEEN REMOVED! Do not use!
-*/
-
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: [
-        'default',
-        {
-          discardComments: {
-            removeAll: true
-          }
-        }
-      ]
-    })
-  ]
+  plugins: {
+    'postcss-preset-env': {
+      browsers: 'last 2 versions',
+    },
+  },
 }
