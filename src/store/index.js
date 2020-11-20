21893785b995adc406a0ab88c2cf93811c479323
Aleksander <ilyasov_as@creacept.ru>
2020-11-20 16:54:11 +0300

    CRM_app

--- src/store/index.js
+++ src/store/index.js
@@ -4,22 +4,5 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    modalView: null
-  },
-  mutations: {
-    setModalView(state, view) {
-      state.modalView = view;
-    }
-  },
-  actions: {
-    setModalView({commit}, view) {
-      commit('setModalView', view);
-    }
-  },
-  getters: {
-    getModalView(state) {
-      return state.modalView
-    }
-  }
+  modules: {}
 })
